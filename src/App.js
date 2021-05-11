45d371c79bd29cf9f633b9420e28f3a3556c7558
Sneha Rajakumar <snehadr23@gmail.com>
2021-05-11 00:16:31 -0700

    added link to the git repo

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
         <h1 className = 'text-primary mb-3 mt-4'>Search Github User</h1>
       </header>
       <Search/>
+      <div>
+        <a className = 'git-repo' target = '_blank' href = 'https://github.com/snehadr23/search-github-user'>
+          <i className = 'fab fa-github'></i>
+        </a>
+      </div>
     </div>
   );
 }
